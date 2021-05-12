72680a694c88d67424403abfc5334d59675414cf
murewaashiru <rachelleashiru@gmail.com>
2021-05-12 22:33:06 +0100

    Removed redundant dependency

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const dotenv = require( 'dotenv');
-const bodyParser = require( 'body-parser');
 const morgan = require( 'morgan');
-// const routes = require( './routes');
 const routes = require( './routes/index');
 dotenv.config();
 
 const app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// app.use('/api/movie', routes);
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 4000;
